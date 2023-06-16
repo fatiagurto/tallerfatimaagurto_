d26835fce9a8fe36fb01594361384b1d3e2f23cb
fatiagurto <136628134+fatiagurto@users.noreply.github.com>
2023-06-16 01:47:46 +0000

    implementacion de codigos

---
+++ programa.py
@@ -0,0 +1,9 @@
+from matematicas import sumar, restar, multiplicar, dividir
+from geometria import distancia
+
+print(sumar(2, 3))
+print(restar(4, 2))
+print(multiplicar(6, 18))
+print(dividir(30, 5))
+
+print("distancia", distancia(1, 2, 4, 5))
